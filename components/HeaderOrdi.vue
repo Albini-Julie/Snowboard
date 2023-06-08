9b8157442081486b5df2eee82ab3964fe0acc422
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-06-08 13:48:21 +0200

    1er jet Header

---
+++ components/HeaderOrdi.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <div>
+            <img src="../src/Logo.png" alt="logo The Snow Border"/>
+            <h3 class="titre-header">Snowboards</h3>
+            <h3>Splitboards</h3>
+            <h3>Boots</h3>
+            <h3>Fixations</h3>
+            <h3>Housses</h3>
+            <h3>Divers</h3>
+        </div>
+        <div>
+            <img src="../src/LoupeIcon.png" alt="icon loupe"/>
+            <img src="../src/AccountIcon.png" alt="icon personne"/>
+            <img src="../src/CartIcon.png" alt="icon carte"/>
+        </div>
+    </div>
+</template>
+
+<style>
+.header{
+    display:flex;
+    justify-content: space-around;
+}
+
+.titre-header{
+    font-family: 'Cabin', sans-serif;
+}
+
+</style>
