3c8995a5eace983fc6ec9af3e793774f0751e979
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-06-09 09:10:58 +0200

    Amélioration des boutons couleur et taille

--- script.js
+++ script.js
@@ -4,27 +4,16 @@ function change() {
         button.style.backgroundColor = "#4374EF";
         let button2 = document.getElementById("button2");
         button2.style.backgroundColor = "#FFFFFF";
+        button.style.color = "#FFFFFF";
+        button2.style.color = "#4374EF";
     }
     else{
         let button = document.getElementById("button");
         button.style.backgroundColor = "#FFFFFF";
         let button2 = document.getElementById("button2");
         button2.style.backgroundColor = "#4374EF";
-    }
-}
-
-function changeText() {
-    if (test2 == true){
-        let text = document.getElementById("text");
-        text.style.color = "#FFFFFF"
-        let text2 = document.getElementById("text2");
-        text2.style.color = "#4374EF"
-    }
-    else{
-        let text = document.getElementById("text");
-        text.style.color = "#4374EF"
-        let text2 = document.getElementById("text2");
-        text2.style.color = "#FFFFFF"
+        button2.style.color = "#FFFFFF";
+        button.style.color = "#4374EF";
     }
 }
 
@@ -34,27 +23,16 @@ function change2() {
         button3.style.backgroundColor = "#4374EF";
         let button4 = document.getElementById("button4");
         button4.style.backgroundColor = "#FFFFFF";
+        button3.style.color = "#FFFFFF";
+        button4.style.color = "#4374EF";
     }
     else{
         let button3 = document.getElementById("button3");
         button3.style.backgroundColor = "#FFFFFF";
         let button4 = document.getElementById("button4");
         button4.style.backgroundColor = "#4374EF";
-    }
-}
-
-function changeText2() {
-    if (test4 == true){
-        let text3 = document.getElementById("text3");
-        text3.style.color = "#FFFFFF"
-        let text4 = document.getElementById("text4");
-        text4.style.color = "#4374EF"
-    }
-    else{
-        let text3 = document.getElementById("text3");
-        text3.style.color = "#4374EF"
-        let text4 = document.getElementById("text4");
-        text4.style.color = "#FFFFFF"
+        button4.style.color = "#FFFFFF";
+        button3.style.color = "#4374EF";
     }
 }
 
